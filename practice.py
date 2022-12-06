c022ce8067a252d6c6d22f8e9003436a21feb914
lydiaworku <lworku@lion.lmu.edu>
2022-12-05 18:49:23 -0800

    adding practice.py

--- practice.py
+++ practice.py
@@ -151,13 +151,6 @@ class BoxRow():
 
 
 
-# BoxRow.firstrow()
-# BoxRow.secondrow()
-# BoxRow.thirdrow()
-# BoxRow.fourthrow()
-# BoxRow.fifthrow()
-
-
 print(word)
 
 # Loop that keeps the game open until the user quits
@@ -204,7 +197,6 @@ def play_game(game):
             game.turn_dark_gray(pygame.Rect((screen_width * 0.5 - (box_width / 2)) + box_width * 3, (screen_height * 0.2) + ((counter - 1) * 75), box_width, box_height))
             pygame.display.update()
         
-        print('after first if statement chunk')
 
 # correct letter incorrect placement
 
@@ -224,7 +216,6 @@ def play_game(game):
             game.turn_yellow(pygame.Rect((screen_width * 0.5 - (box_width / 2)) + box_width * 3, (screen_height * 0.2) + ((counter - 1) * 75), box_width, box_height))
             pygame.display.update()
 
-        print('after second if statement chunk')
 
 # correct letter and placement
 
@@ -244,35 +235,34 @@ def play_game(game):
             game.turn_green(pygame.Rect((screen_width * 0.5 - (box_width / 2)) + box_width * 3, (screen_height * 0.2) + ((counter - 1) * 75), box_width, box_height))
             pygame.display.update()
 
-        print('after third if statement chunk')
 
 
         letter1 = font.render(user_word[0], True, black, None)
         letter_rect1 = letter1.get_rect(center = (screen_width / 4.77 , screen_height / 4 + (counter - 1) * 72))
         screen.blit(letter1, letter_rect1)
+        pygame.display.update()
 
         letter2 = font.render(user_word[1], True, black, None)
         letter_rect2 = letter2.get_rect(center = (screen_width / 2.9 , screen_height / 4 + (counter - 1) * 72))
         screen.blit(letter2, letter_rect2)
+        pygame.display.update()
 
         letter3 = font.render(user_word[2], True, black, None)
         letter_rect3 = letter3.get_rect(center = (screen_width / 2 , screen_height / 4 + (counter - 1) * 72))
         screen.blit(letter3, letter_rect3)
+        pygame.display.update()
 
         letter4 = font.render(user_word[3], True, black, None)
         letter_rect4 = letter4.get_rect(center = (screen_width / 1.53 , screen_height / 4 + (counter - 1) * 72))
         screen.blit(letter4, letter_rect4)
+        pygame.display.update()
 
         letter5 = font.render(user_word[4], True, black, None)
         letter_rect5 = letter5.get_rect(center = (screen_width / 1.25 , screen_height / 4 + (counter - 1) * 72))
         screen.blit(letter5, letter_rect5)
-
-        print('after the letters')
-
-        
         pygame.display.update()
 
-        print('after display update')
+        
 
         return True
 
