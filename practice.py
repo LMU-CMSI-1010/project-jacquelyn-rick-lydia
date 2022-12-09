cc8099405faf4acb5b1c657ee61c4b7c1669f814
lydiaworku <lworku@lion.lmu.edu>
2022-12-09 13:14:13 -0800

    adding documented code

--- practice.py
+++ practice.py
@@ -2,11 +2,11 @@ import pygame
 import sys
 
 # initializing the game
+
 pygame.init()
 pygame.event.get()
 
-
-clock = pygame.time.Clock()
+# setting up colors
 
 green = (0, 200, 0)
 blue = (0, 0, 200)
@@ -17,7 +17,7 @@ light_gray = (200, 200, 200)
 yellow = (255, 235, 0)
 dark_gray = (135, 135, 135)
 
-
+# setting screen and box widths and heights
 
 screen_width = 600
 screen_height = 600
@@ -26,11 +26,12 @@ screen_height = 600
 box_width = screen_width / 10
 box_height = screen_height / 10
 
+# sets up the white screen
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 screen.fill(white)
 
-# Centers the text and puts it onto the screen
+# centers the text and puts it onto the screen
 
 pygame.display.set_caption('Wordle Game')
 
@@ -41,9 +42,11 @@ text_rect = text.get_rect(center = (screen_width / 2, screen_height / 10))
 screen.blit(text, text_rect)
 pygame.display.update()
 
-# Picks a word from the random list of english words 
+# importing a list of lowercase english words with no punctuation from the english_words library
 
 from english_words import english_words_lower_alpha_set
+
+# creating a list of five-letter words from the list of lowercase words
 
 five_list = []
 
@@ -52,7 +55,7 @@ for item in english_words_lower_alpha_set:
         word = item
         five_list.append(item)
 
-# Creates the rows and boxes
+# creates the rows of boxes -- six rows, five boxes in each row
 
 r1b3 = pygame.Rect((screen_width * 0.5 - (box_width / 2)), (screen_height * 0.2), box_width, box_height)
 r1b2 = pygame.Rect((screen_width * 0.5 - (box_width / 2)) - box_width * 1.5, (screen_height * 0.2), box_width, box_height)
@@ -153,22 +156,25 @@ class BoxRow():
         pygame.draw.rect(screen, light_gray, r6b5, 3)
         pygame.display.update()
 
+# turns box green
 
     def turn_green(self, box):
         pygame.draw.rect(screen, green, box, 0)
-        print('turn green function is running')
         pygame.display.update()
+
+# turns box yellow
 
     def turn_yellow(self, box):
         pygame.draw.rect(screen, yellow, box, 0)
-        print('turn yellow function is running')
         pygame.display.update()
+
+# turns box dark gray
 
     def turn_dark_gray(self, box):
         pygame.draw.rect(screen, dark_gray, box, 0)
-        print('turn dark gray function is running')
         pygame.display.update()
 
+# displays a congrats message to the pygame screen -- used if the user guess the word correctly
 
     def congrats_message(self):
         pygame.time.delay(1500)
@@ -182,10 +188,11 @@ class BoxRow():
             congrats = cong_font.render('Congrats! You guessed the word in ' + str(counter) + ' tries.', True, black, None)
         cong_rect = congrats.get_rect(center = (screen_width / 2, screen_height / 3))
         screen.blit(congrats, cong_rect)
-        print('congrats')
         pygame.display.update()
         pygame.time.delay(4000)
         pygame.quit()
+
+# displays a message to the screen -- used if the user runs out of their six tries and hasn't gotten the word
 
     def out_of_tries(self):
         pygame.time.delay(3000)
@@ -196,13 +203,13 @@ class BoxRow():
         out = out_font.render('Out of tries! The correct word was: "' + word + '".', True, black, None)
         out_rect = out.get_rect(center = (screen_width / 2, screen_height / 3))
         screen.blit(out, out_rect)
-        print('out of tries')
         pygame.display.update()
         pygame.time.delay(2000)
         pygame.quit()
 
+# displays a message to the screen -- used if the user quits the game before using all their tries or guessing the word correctly
+
     def give_up(self):
-        print('give up?')
         pygame.time.delay(500)
         cong_screen = pygame.Rect(screen_width / 16, screen_height / 4, screen_width * 0.9, screen_height * 0.16)
         pygame.draw.rect(screen, white, cong_screen, 0)
@@ -217,29 +224,42 @@ class BoxRow():
         sys.exit()
 
 
-
-
-
-# Loop that keeps the game open until the user quits
-
-
-
-
+# play_game function -- this is where the functionality of the game comes from
 
 def play_game(game):
 
-    user_word = input('what is your guess? ')
+    user_word = input('Guess #' + str(counter) + ': ')
+
+# user's guess should not include numbers or symbols
+
+    if '1' in user_word or '2' in user_word or '3' in user_word or '4' in user_word or '5' in user_word or '6' in user_word or '7' in user_word or '8' in user_word or '9' in user_word or '0' in user_word:
+        print('Guess must be a five letter word. ')
+        wrong_word = "wrong word"
+        return wrong_word
+    
+    if '!' in user_word or '@' in user_word or '#' in user_word or '$' in user_word or '%' in user_word or '^' in user_word or '&' in user_word or '*' in user_word or '(' in user_word or '0' in user_word:
+        print('Guess must be a five letter word. ')
+        wrong_word = "wrong word"
+        return wrong_word
+
+    if '`' in user_word or '~' in user_word or '-' in user_word or '_' in user_word or '+' in user_word or '=' in user_word or '[' in user_word or ']' in user_word or '{' in user_word or '}' in user_word:
+        print('Guess must be a five letter word. ')
+        wrong_word = "wrong word"
+        return wrong_word
+
+# if user puts 'q' as their guess, the game quits and runs the give_up() method
+# if user's guess is less than 5 letters and not 'q', an error message is displayed in the terminal
 
     if len(user_word) != 5:
         if user_word == 'q' or user_word == 'Q':
             game.give_up()
         else:
-            print('guess must be a five letter word')
+            print('Guess must be a five letter word. ')
             wrong_word = "wrong word"
             return wrong_word
     else:
 
-# incorrect letter
+# if the user's guess includes an incorrect letter, the turn_dark_gray() method is run for the corresponding box
 
         if user_word[0] not in word:
             game.turn_dark_gray(pygame.Rect((screen_width * 0.5 - (box_width / 2)) - box_width * 3, (screen_height * 0.2) + ((counter - 1) * 75), box_width, box_height))
@@ -263,7 +283,7 @@ def play_game(game):
             pygame.time.delay(10)
         
 
-# correct letter incorrect placement
+# if the user's guess includes an correct letter in the wrong spot, the turn_yellow() method is run for the corresponding box
 
         if user_word[0] != word[0] and user_word[0] in word:
            game.turn_yellow(pygame.Rect((screen_width * 0.5 - (box_width / 2)) - box_width * 3, (screen_height * 0.2) + ((counter - 1) * 75), box_width, box_height))
@@ -287,7 +307,7 @@ def play_game(game):
             pygame.time.delay(10)
 
 
-# correct letter and placement
+# if the user's guess includes an correct letter in the correct spot, the turn_green() method is run for the corresponding box
 
         if user_word[0] == word[0]:
            game.turn_green(pygame.Rect((screen_width * 0.5 - (box_width / 2)) - box_width * 3, (screen_height * 0.2) + ((counter - 1) * 75), box_width, box_height))
@@ -310,6 +330,7 @@ def play_game(game):
             pygame.display.update()
             pygame.time.delay(10)
 
+# each letter in the user's guess is displayed onto the screen to the row corresponding to the guess they are on
 
         letter1 = font.render(user_word[0], True, black, None)
         letter_rect1 = letter1.get_rect(center = (screen_width / 4.77 , screen_height / 3.8 + (counter - 1) * 72))
@@ -341,6 +362,8 @@ def play_game(game):
         pygame.display.update()
         pygame.time.delay(10)
 
+# if the user guesses the word correctly, the congrats_message() method is run
+
         if user_word == word:
             game.congrats_message()
             return False    
@@ -352,25 +375,33 @@ def play_game(game):
 pygame.display.update()
 
 
-
+# the counter is started as 1
 
 counter = 1
 playing = True
 game = BoxRow()
+
+# while loop that keeps the game playing
+
 while playing:
 
-    if counter < 8:
-        print(counter)
+
+# if the counter less than 7 (meaning the user has more tries left), the play_game() function is run
+# if the user's guess is not valid (too many letters, contains numbers, etc.), the counter is not incremented and the user gets to redo that try
+# otherwise, the counter is incremented and the user moves on to the next try
+
+    if counter < 7:
         playing = play_game(game)
         if playing == "wrong word":
             counter = counter
         else:
             counter += 1
-    
+
+# if the counter is equal to 7 (meaning the user has used up their last try), the out_of_tries() method is run and the game quits
+
     if counter == 7:
         if playing == True:
             game.out_of_tries()
 
         pygame.quit()
         exit()
-
